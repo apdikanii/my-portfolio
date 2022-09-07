6c3afeb7dd691274a86fe0cc242e35dbd18d4956
Abdikani Osman <abdikanilibaxa@gmail.com>
2022-09-07 07:04:28 -0600

    third commit

---
+++ js/custom.js
@@ -0,0 +1,89 @@
+
+$(document).ready(function(){
+
+    $('.servise-con').slick({
+     slidesToShow: 2,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+        dots: false,
+        arrows: false,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: false
+          }
+        },
+        {
+          breakpoint: 1119,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+
+    $('.sliders-testimonial').slick({
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+        dots: false,
+        arrows: false,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: false
+          }
+        },
+        {
+          breakpoint: 991,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+        
+    });
+    
+});
+var MenuItems = document.getElementById('MenuItems');
+MenuItems.style.maxHeight = "0px";
+function menutoggle(){
+  if(MenuItems.style.maxHeight =="0px"){
+    MenuItems.style.maxHeight = "200px";
+  }
+  else{
+    MenuItems.style.maxHeight = "0px";
+  }
+}
